612bdaaa180827eb2ba150369ef72d8aa916ce65
VeronicaManchola <veronica.manchola@hotmail.com>
2019-05-17 17:15:03 -0400

    Creacion de proyecto en react

---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+const settings = {};
+
+const config = {
+    apiKey: "AIzaSyDUGgco_94yayaUfPyfF9HqDc4iJZkYTsc",
+    authDomain: "hackatonfinal-3724f.firebaseapp.com",
+    databaseURL: "https://hackatonfinal-3724f.firebaseio.com",
+    projectId: "hackatonfinal-3724f",
+    storageBucket: "hackatonfinal-3724f.appspot.com",
+    messagingSenderId: "1042219548735",
+    appId: "1:1042219548735:web:ff4f131b0bf63bc0"
+};
+
+firebase.initializeApp(config);
+
+firebase.database().settings(settings);
+
+export default firebase;

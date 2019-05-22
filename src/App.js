e1288bdff0f929349b77eb0b848fc22740bcdab1
LeslieSaVa <leslie.sabattin@usach.cl>
2019-05-22 08:59:01 -0400

    mapa con zoom y pointer ubicacion actual

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Map from './components/map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      app_id: "fhk2odOlobSO5rRWPQ73",
+      app_code: "BgS4fH56ONRVytxVXgfF0w",
+    }
+  }
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.setState({
+            ...this.state,
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            error: null,
+          });
+        },
+        (error) => {
+          this.setState({
+            ...this.state,
+            error: error.message
+          })
+        }
+      );
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <Map 
+        app_id={this.state.app_id}
+        app_code={this.state.app_code}
+        zoom="14"
+        lat={this.state.lat ? this.state.lat : "-33.4489"}
+        lng={this.state.lng ? this.state.lng : "-70.6693"}
+        marker={this.state.marker}
+        />
+
+      </div>
+    )
+  }
+
 }
-
-export default App;

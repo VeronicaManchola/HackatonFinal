86d53970563f3078342f44b6de411fac439cc797
VeronicaManchola <veronica.manchola@hotmail.com>
2019-05-23 23:55:06 -0400

    merge alineado con Pulie

--- src/Components/Route/HwgMap.js
+++ src/Components/Route/HwgMap.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import BottomBtn from '../HomeScreen/BottomBtn';
 
 class HgwMap extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -12,19 +13,22 @@ class HgwMap extends Component {
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         let address = this.props.location.state.address;
         let splitAddress = address.split(" ")
         let joinAddress = splitAddress.join("-");
         this.setState({ endPoint: joinAddress });
     }
 
-    render(){
-        return(
-            <iframe 
-            width="100%" 
-            height="600" 
-            src={`https://wego.here.com/directions/mix/Calle-Puma,-8420000-Recoleta,-Recoleta,-Region-Santiago-Metropolitan,-Chile:loc-dmVyc2lvbj0xO3RpdGxlPUNhbGxlK1B1bWE7bGFuZz1lcztsYXQ9LTMzLjQxOTQ5ODQ0MzYwMzUxNjtsb249LTcwLjY0MjAyODgwODU5Mzc1O3N0cmVldD1DYWxsZStQdW1hO2NpdHk9UmVjb2xldGE7cG9zdGFsQ29kZT04NDIwMDAwO2NvdW50cnk9Q0hMO2Rpc3RyaWN0PVJlY29sZXRhO3N0YXRlPVJlZ2lvbitTYW50aWFnbytNZXRyb3BvbGl0YW47c3RhdGVDb2RlPVJNO2NvdW50eT1TYW50aWFnbztjYXRlZ29yeUlkPXN0cmVldC1zcXVhcmU7c291cmNlU3lzdGVtPWludGVybmFsO3Bkc0NhdGVnb3J5SWQ9OTAwLTk0MDAtMDQwMQ/${this.state.endPoint}?map=${this.state.lat},${this.state.lng},15,normal`}></iframe>
+    render() {
+        return (
+            <div>
+                <iframe
+                    width="100%"
+                    height="600"
+                    src={`https://mobile.here.com/directions/Calle-Puma:-33.419498,-70.642029/${this.state.endPoint}:${this.state.lat},${this.state.lng}?msg=${this.state.endPoint}`}></iframe>
+                <BottomBtn />
+            </div>
         )
     }
 }
